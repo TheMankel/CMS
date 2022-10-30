a450fb55cf52294898cc3ad6679f5770ca01a0cd
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-10-30 18:07:44 +0100

    Created Sidebar with archives and social medias

---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+function Sidebar(props) {
+  const { archives, social } = props;
+
+  const socialProvided = social.some((network) => network.url !== '');
+
+  return (
+    <Grid item xs={12} md={4}>
+      <Typography variant='h6' gutterBottom>
+        Archives
+      </Typography>
+      {archives.map((archive) => (
+        <Link
+          display='block'
+          variant='body1'
+          href={archive.url}
+          key={archive.title}>
+          {archive.title}
+        </Link>
+      ))}
+      {socialProvided && (
+        <Typography variant='h6' gutterBottom sx={{ mt: 3 }}>
+          Social
+        </Typography>
+      )}
+      {social.map(
+        (network) =>
+          network.url && (
+            <Link
+              display='block'
+              variant='body1'
+              href={network.url}
+              key={network.name}
+              sx={{ mb: 0.5 }}>
+              <Stack direction='row' spacing={1} alignItems='center'>
+                <network.icon />
+                <span>{network.name}</span>
+              </Stack>
+            </Link>
+          ),
+      )}
+    </Grid>
+  );
+}
+
+Sidebar.propTypes = {
+  archives: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  description: PropTypes.string.isRequired,
+  social: PropTypes.arrayOf(
+    PropTypes.shape({
+      icon: PropTypes.elementType.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default Sidebar;
