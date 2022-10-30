96e41c98993105d79464192a24a973f5ce2aaae6
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-10-30 16:21:29 +0100

    Added React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+import HomePage from './views/HomePage';
+import NotFound from './views/NotFound';
+
 function App() {
-  return;
+  return (
+    <Routes>
+      <Route path='/' element={<Navigate replace to='/home' />} />
+      <Route path='/home' element={<HomePage />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
