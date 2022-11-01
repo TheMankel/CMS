958940b09afe3847a4ab59e754e0a2c9880e68e3
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-01 14:38:01 +0100

    Added dashboard to routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 
 import HomePage from './views/HomePage';
+import Dashboard from './views/admin/Dashboard';
 import NotFound from './views/NotFound';
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <Routes>
       <Route path='/' element={<Navigate replace to='/home' />} />
       <Route path='/home' element={<HomePage />} />
+      //
+      <Route path='/dashboard' element={<Dashboard />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   );
