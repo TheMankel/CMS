ba096bb3048e369397e75ecb67aeb31562e8cd87
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-02 10:23:10 +0100

    Added login and register routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './views/HomePage';
 import Dashboard from './views/admin/Dashboard';
 import NotFound from './views/NotFound';
+import SignIn from './views/SignIn';
+import SignUp from './views/SignUp';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigate replace to='/home' />} />
       <Route path='/home' element={<HomePage />} />
+      <Route path='/login' element={<SignIn />} />
+      <Route path='/register' element={<SignUp />} />
       <Route path='/dashboard' element={<Dashboard />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
