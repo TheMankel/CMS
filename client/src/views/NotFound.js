ef7050a3bc12845ad3ba98c438451a27c9a1af4b
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-04 11:26:17 +0100

    Refactor
    Added card content and actions

--- client/src/views/NotFound.js
+++ client/src/views/NotFound.js
@@ -1,44 +1,65 @@
 import React from 'react';
 
-import { Box, Typography } from '@mui/material';
-import { grey } from '@mui/material/colors';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { grey } from '@mui/material/colors';
 
 const NotFound = () => {
   const primary = grey[500];
   const secondary = grey[800];
   const hover = grey[900];
   return (
-    <>
-      <Box
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        backgroundColor: primary,
+      }}>
+      <Card
         sx={{
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-          minHeight: '100vh',
-          backgroundColor: primary,
-          color: 'white',
+          borderRadius: '16px',
         }}>
-        <Typography variant='h1'>404</Typography>
-        <Typography variant='h6'>
-          The page you're looking for doesn't exist.
-        </Typography>
-        <Button
-          variant='contained'
-          href='/home'
-          color='inherit'
+        <CardContent
           sx={{
-            mt: 2,
-            backgroundColor: secondary,
-            ':hover': {
-              backgroundColor: hover,
-            },
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-around',
+            gap: 2,
+            textAlign: 'center',
           }}>
-          Home
-        </Button>
-      </Box>
-    </>
+          <Typography variant='h1'>404</Typography>
+          <Typography variant='h6'>
+            The page you're looking for doesn't exist.
+          </Typography>
+        </CardContent>
+        <CardActions sx={{ padding: '16px' }}>
+          <Button
+            variant='contained'
+            href='/home'
+            color='inherit'
+            sx={{
+              color: 'white',
+              backgroundColor: secondary,
+              ':hover': {
+                backgroundColor: hover,
+              },
+            }}>
+            Home
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
   );
 };
 
