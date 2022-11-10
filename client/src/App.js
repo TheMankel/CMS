aa588d963164b43fd12ff795b76625bb0f7e3400
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-10 22:07:35 +0100

    Added authProvider to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-
+import { AuthProvider } from './contexts/authContext';
 import HomePage from './views/HomePage';
 import Dashboard from './views/admin/Dashboard';
 import NotFound from './views/NotFound';
@@ -8,14 +8,16 @@ import SignUp from './views/SignUp';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Navigate replace to='/home' />} />
-      <Route path='/home' element={<HomePage />} />
-      <Route path='/login' element={<SignIn />} />
-      <Route path='/register' element={<SignUp />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path='/' element={<Navigate replace to='/home' />} />
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/login' element={<SignIn />} />
+        <Route path='/register' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
