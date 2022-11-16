51ae0c357443ea521a986880e143479bff8144f6
HiImWhite <wojttasek1999@gmail.com>
2022-11-16 21:32:43 +0100

    Finishing touches to About view

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
-import Headline from "../components/Headline/Headline";
-import Numbers from "../components/Numbers/Numbers";
 import Story from "../components/Story/Story";
 import Team from "../components/Team/Team";
 import MainPublic from "../layouts/MainPublic";
@@ -11,18 +9,9 @@ const About = () => {
   return (
     <MainPublic>
       <Box my={3}>
-        <Headline />
+        <Story />
       </Box>
       <Divider />
-      <Box my={3}>
-        <Story />
-        <Box my={3}>
-          <Numbers />
-          <Box my={3}>
-            <Divider />
-          </Box>
-        </Box>
-      </Box>
       <Box my={3}>
         <Team />
       </Box>
