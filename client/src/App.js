8bfff21ba576e4f7b4b8a0782695efcc872ccf74
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-16 23:01:17 +0100

    Fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import { AuthProvider } from "./contexts/authContext";
-import HomePage from "./views/HomePage";
-import Dashboard from "./views/admin/Dashboard";
-import NotFound from "./views/NotFound";
-import SignIn from "./views/SignIn";
-import SignUp from "./views/SignUp";
-import ProfilePage from "./views/ProfilePage";
-import About from "./views/About";
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/authContext';
+import HomePage from './views/HomePage';
+import Dashboard from './views/admin/Dashboard';
+import NotFound from './views/NotFound';
+import SignIn from './views/SignIn';
+import SignUp from './views/SignUp';
+// import ProfilePage from "./views/ProfilePage";
+import About from './views/About';
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/" element={<Navigate replace to="/home" />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/test" element={<ProfilePage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path='/' element={<Navigate replace to='/home' />} />
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/login' element={<SignIn />} />
+        <Route path='/register' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/about' element={<About />} />
+        {/* <Route path="/test" element={<ProfilePage />} /> */}
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </AuthProvider>
   );
