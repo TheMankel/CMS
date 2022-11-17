1c2d8dc0e20a5c018ccfa544d2969427d4d7b887
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-17 23:23:26 +0100

    Applied AuthProvider to the entire application

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,42 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import { AuthProvider } from "./contexts/authContext";
-import HomePage from "./views/HomePage";
-import Dashboard from "./views/admin/Dashboard";
-import NotFound from "./views/NotFound";
-import SignIn from "./views/SignIn";
-import SignUp from "./views/SignUp";
+import { Route, Routes, Navigate } from 'react-router-dom';
+import HomePage from './views/HomePage';
+import Dashboard from './views/admin/Dashboard';
+import NotFound from './views/NotFound';
+import SignIn from './views/SignIn';
+import SignUp from './views/SignUp';
 // import ProfilePage from "./views/ProfilePage";
-import About from "./views/About";
-import Contact from "./views/Contact";
+import About from './views/About';
+import Contact from './views/Contact';
+import PrivateRoute from './utils/PrivateRoute';
+import { useAuth } from './contexts/authContext';
 
 function App() {
+  const { user, role } = useAuth();
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/home" />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/test" element={<ProfilePage />} /> */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </AuthProvider>
+    <Routes>
+      <Route path='/' element={<Navigate replace to='/home' />} />
+      <Route path='/home' element={<HomePage />} />
+      <Route path='/login' element={<SignIn />} />
+      <Route path='/register' element={<SignUp />} />
+      <Route
+        path='dashboard'
+        element={
+          <PrivateRoute redirectPath='/home' isAllowed={!!user && role}>
+            {/* {console.log(!!user && role === 'admin')} */}
+            {console.log(role)}
+            {console.log(!!user)}
+            {/* {console.log(role === 'admin')} */}
+            {/* {console.log(!!user)} */}
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      {/* <Route path="/test" element={<ProfilePage />} /> */}
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { AuthProvider } from './contexts/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
 );
