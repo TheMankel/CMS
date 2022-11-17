8e749a2650ae939e965902b0a1ebb99c9ef2215d
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-17 13:57:26 +0100

    Update authRoute.js

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { signUp } = require('../controllers/authController');
+const { signUp, signIn } = require('../controllers/authController');
 
 router.post('/signup', signUp);
+router.post('/signin', signIn);
 
 module.exports = router;
