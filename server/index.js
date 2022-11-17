31396a47fb95e87b5a1c0172c85594b4a2d6f754
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-17 13:57:14 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,9 +12,15 @@ const port = process.env.PORT || 8000;
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  }),
+);
 app.use('/api', authRoutes);
 
 app.listen(port, () =>
