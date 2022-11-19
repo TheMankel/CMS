ea0ff2033348eb80bf680cd1abf3e8cf22250da7
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-19 09:17:47 +0100

    Added svg illustration to contact view

--- client/src/views/Contact.js
+++ client/src/views/Contact.js
@@ -1,21 +1,62 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Form from "../components/Form/Form";
-import MainPublic from "../layouts/MainPublic";
-import FormText from "../components/Form/FormText";
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Form from '../components/Form/Form';
+import MainPublic from '../layouts/MainPublic';
+import MailWriterIllustration from '../svg/MailWriter';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 const Contact = () => {
   return (
     <MainPublic>
-      <Box my={3}>
-        <FormText />
-      </Box>
-      <Grid align="center" spacing={24}>
-        <Grid my={5} container direction="column" maxWidth={"60vh"}>
-          <Form />
+      <Container>
+        <Grid container spacing={6} my={3}>
+          <Grid item container justifyContent={'center'} xs={12} md={6}>
+            <Box
+              height={'100%'}
+              width={'100%'}
+              maxWidth={{ xs: 500, md: '100%' }}>
+              <Box marginBottom={4}>
+                <Box
+                  component={Typography}
+                  fontWeight={700}
+                  variant={'h5'}
+                  gutterBottom
+                  align={'center'}>
+                  CONTACT US
+                </Box>
+                <Typography
+                  variant={'h6'}
+                  component={'p'}
+                  color={'textSecondary'}
+                  align={'center'}>
+                  We would love to talk about how we can help you.
+                </Typography>
+              </Box>
+              <Box marginBottom={4}>
+                <MailWriterIllustration width={'100%'} height={'100%'} />
+              </Box>
+              <Box>
+                <Typography color='text.secondary' align={'center'}>
+                  We'll get back to you in 1-2 business days.
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            container
+            alignItems={'center'}
+            justifyContent={'center'}
+            xs={12}
+            md={6}>
+            <Box>
+              <Form />
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      </Container>
     </MainPublic>
   );
 };
