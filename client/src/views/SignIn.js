8de3c8b787d23f00fdc60d0ef174155a9d33cfe0
HiImWhite <wojttasek1999@gmail.com>
2022-11-20 23:52:44 +0100

    Link forgot

--- client/src/views/SignIn.js
+++ client/src/views/SignIn.js
@@ -139,7 +139,7 @@ const SignIn = () => {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link href='#' variant='body2'>
+                  <Link href='/forgotpassword' variant='body2'>
                     Forgot password?
                   </Link>
                 </Grid>
