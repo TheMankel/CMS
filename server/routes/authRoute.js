06089628ddfe657b31d26a1bb66d3c74dfea77a4
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-21 19:02:16 +0100

    Added navigation server route

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { signUp, signIn } = require('../controllers/authController');
+const { signUp, signIn, navigation } = require('../controllers/authController');
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
+router.get('/navigation', navigation);
 
 module.exports = router;
