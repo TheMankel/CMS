f746cebf3f479955baeda5a2278fa6c83bfd9faa
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-21 18:58:11 +0100

    Added getting layout data from firestore

--- client/src/layouts/MainPublic.js
+++ client/src/layouts/MainPublic.js
@@ -1,49 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
-import { useAuth } from '../contexts/authContext';
+import axios from 'axios';
 
 const MainPublic = (props) => {
-  const { user } = useAuth();
+  const [sections, setSections] = useState([{ title: '', url: '' }]);
+  const [categories, setCategories] = useState([{ title: '', links: [''] }]);
+  // const [loading, setLoading] = useState(true);
   const theme = createTheme();
 
-  const sections = [
-    { title: 'Technology', url: '#' },
-    { title: 'Design', url: '#' },
-    { title: 'Culture', url: '#' },
-    { title: 'Business', url: '#' },
-    { title: 'Politics', url: '#' },
-    { title: 'Opinion', url: '#' },
-    { title: 'Science', url: '#' },
-    { title: 'Health', url: '#' },
-    { title: 'Style', url: '#' },
-    { title: 'Travel', url: '#' },
-  ];
+  useEffect(() => {
+    getData();
+  }, []);
 
-  const logo = 'https://source.unsplash.com/random';
+  const getData = async () => {
+    try {
+      const data = await axios.get('http://localhost:8000/api/homepage');
+      setCategories(data?.data?.categories);
+      setSections(data?.data?.sections);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  const categories = [
-    {
-      title: 'category1',
-      links: ['link1', 'link2', 'link3', 'link4'],
-    },
-    {
-      title: 'category2',
-      links: ['link1', 'link2', 'link3', 'link4'],
-    },
-    {
-      title: 'category3',
-      links: ['link1', 'link2', 'link3', 'link4'],
-    },
-  ];
+  // const sections = [
+  //   { title: 'Technology', url: '#' },
+  //   { title: 'Design', url: '#' },
+  //   { title: 'Culture', url: '#' },
+  //   { title: 'Business', url: '#' },
+  //   { title: 'Politics', url: '#' },
+  //   { title: 'Opinion', url: '#' },
+  //   { title: 'Science', url: '#' },
+  //   { title: 'Health', url: '#' },
+  //   { title: 'Style', url: '#' },
+  //   { title: 'Travel', url: '#' },
+  // ];
+
+  // const categories = [
+  //   {
+  //     title: 'category1',
+  //     links: ['link1', 'link2', 'link3', 'link4'],
+  //   },
+  //   {
+  //     title: 'category2',
+  //     links: ['link1', 'link2', 'link3', 'link4'],
+  //   },
+  //   {
+  //     title: 'category3',
+  //     links: ['link1', 'link2', 'link3', 'link4'],
+  //   },
+  // ];
+
+  // const logo = 'https://source.unsplash.com/random';
+  const logo =
+    'https://seeklogo.com/images/B/blogger_B-logo-47610B2F87-seeklogo.com.png';
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header user={user} title='Blog' logo={logo} sections={sections} />
-      {props.children}
+      <Header title='Blog' logo={logo} sections={sections} />
+      {/* {props.children} */}
+      <Outlet />
       <Footer title='Blog' categories={categories} />
     </ThemeProvider>
   );
