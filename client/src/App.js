09b9bca92847c57cc691837a32cae03aa48b54c1
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-21 19:00:42 +0100

    Added nested routes with layout wrapper
    Adding a wrapper over the routes fixed the problem of re-rendering the component every time the route was changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { useAuth } from './contexts/authContext';
 import HomePage from './views/HomePage';
 import Dashboard from './views/admin/Dashboard';
 import NotFound from './views/NotFound';
@@ -7,39 +8,41 @@ import SignUp from './views/SignUp';
 import ProfilePage from './views/ProfilePage';
 import About from './views/About';
 import Contact from './views/Contact';
-import PrivateRoute from './utils/PrivateRoute';
-import { useAuth } from './contexts/authContext';
 import ForgotPassword from './views/ForgotPassword';
+import PrivateRoute from './utils/PrivateRoute';
+import MainPublic from './layouts/MainPublic';
 
 function App() {
   const { user, role } = useAuth();
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/login' element={<SignIn />} />
-      <Route path='/register' element={<SignUp />} />
-      <Route path='/forgotpassword' element={<ForgotPassword />} />
-      <Route
-        path='dashboard'
-        element={
-          <PrivateRoute redirectPath='/' permissions={!!user && role}>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route
-        path='account'
-        element={
-          <PrivateRoute redirectPath='/login' permissions={!!user}>
-            <ProfilePage />
-          </PrivateRoute>
-        }
-      />
-      {/* <Route path='/account' element={<ProfilePage />} /> */}
-      <Route path='*' element={<NotFound />} />
+      <Route element={<MainPublic />}>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route
+          path='/account'
+          element={
+            <PrivateRoute redirectPath='/login' permissions={!!user}>
+              <ProfilePage />
+            </PrivateRoute>
+          }
+        />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+      <Route>
+        <Route path='/login' element={<SignIn />} />
+        <Route path='/register' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route
+          path='dashboard'
+          element={
+            <PrivateRoute redirectPath='/' permissions={!!user && role}>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Route>
     </Routes>
   );
 }
