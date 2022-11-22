2ef2e301a72de8ce7e0aa682d336b1e68e656db5
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-22 21:41:25 +0100

    Added getting about data from firestore

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Story from '../components/Story/Story';
 import Team from '../components/Team/Team';
-// import axios from 'axios';
+import axios from 'axios';
 
 const About = () => {
   const [primaryText, setPrimaryText] = useState('');
@@ -23,29 +23,19 @@ const About = () => {
 
   const getData = async () => {
     try {
-      // const data = await axios.get('http://localhost:8000/api/navigation');
-      setPrimaryText('We travel around the world and blog about it!');
-      setSecondaryText(
-        'We were in every continent on the world and made a movie about it! We are also blogging every bit of our life here for five years already.',
-      );
-      setAuthors([
-        {
-          name: 'Jankos',
-          title: 'Senior',
-          avatar:
-            'https://play-lh.googleusercontent.com/O8mvDQlw4AwmGfUrh4lviZD_PwwhRHz2etA25F77SbXrm3qEHOt2826aNkKar4D0yw',
-          about:
-            'I am an ambitious workaholic, but apart from that, pretty simple person.',
-        },
-        {
-          name: 'Wojtas',
-          title: 'Kozak',
-          avatar:
-            'https://wi.wallpapertip.com/wsimgs/10-107567_cat-wallpaper-funny-cat-desktop.jpg',
-          about:
-            'I am non ambitious workaholic, but apart from that, pretty complicated person.',
-        },
-      ]);
+      const data = await axios.get('http://localhost:8000/api/about');
+      const avatars = [
+        'https://play-lh.googleusercontent.com/O8mvDQlw4AwmGfUrh4lviZD_PwwhRHz2etA25F77SbXrm3qEHOt2826aNkKar4D0yw',
+        'https://wi.wallpapertip.com/wsimgs/10-107567_cat-wallpaper-funny-cat-desktop.jpg',
+      ];
+      // console.log(data);
+      setPrimaryText(data?.data.storyText.primary);
+      setSecondaryText(data?.data.storyText.secondary);
+
+      const authorsData = [...data?.data.team];
+      authorsData.forEach((author, i) => (author.avatar = avatars[i]));
+
+      setAuthors(authorsData);
     } catch (err) {
       console.log(err);
     }
