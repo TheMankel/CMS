3912e6972e6b5103e9cafff8d529e93514290cf9
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-22 21:41:43 +0100

    Added about server route

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { signUp, signIn, navigation } = require('../controllers/authController');
+const {
+  signUp,
+  signIn,
+  navigation,
+  about,
+} = require('../controllers/authController');
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
 router.get('/navigation', navigation);
+router.get('/about', about);
 
 module.exports = router;
