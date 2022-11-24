6394428ef4441065bb86cda88d8c12b8e243219b
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-24 17:15:37 +0100

    Added StorageProvider to whole app

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './contexts/authContext';
+import { StorageProvider } from './contexts/storageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <StorageProvider>
+          <App />
+        </StorageProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
