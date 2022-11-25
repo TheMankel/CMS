ab00aac05e92eb21913f28b3b42d59756f67f70c
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-11-25 11:29:09 +0100

    Changed storage from context to lib

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './contexts/authContext';
-import { StorageProvider } from './contexts/storageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <StorageProvider>
-          <App />
-        </StorageProvider>
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ client/src/lib/storage.js
@@ -0,0 +1,37 @@
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+} from 'firebase/storage';
+import app from '../config/firebase-config';
+
+const storage = getStorage(app);
+
+export const createRef = (url) => {
+  return ref(storage, url);
+};
+
+export const uploadImage = async (ref, imageFile) => {
+  await uploadBytes(ref, imageFile);
+};
+
+export const downloadImage = async (ref, callback) => {
+  const imageUrl = await getDownloadURL(ref);
+  callback(imageUrl);
+};
+
+export const getListImages = async (ref, callback) => {
+  const listImages = await listAll(ref);
+  callback(listImages.items);
+};
+
+export const downloadImages = async (ref, callback) => {
+  const listImages = await listAll(ref);
+
+  listImages.items.forEach(async (image) => {
+    const imageUrl = await getDownloadURL(image);
+    callback((prev) => [...prev, imageUrl]);
+  });
+};
