57d7d58b6079e00f1bf8fba98dba416bad2f5743
HiImWhite <wojttasek1999@gmail.com>
2022-11-27 20:59:50 +0100

    Create contactController.js

---
+++ server/controllers/contactController.js
@@ -0,0 +1,50 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  //   host: 'smtp.gmail.com',
+  //   port: 587,
+  service: 'Gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take our messages');
+  }
+});
+
+const contact = (req, res, next) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const msg = req.body.msg;
+
+  const mail = {
+    from: {
+      name: firstName,
+      address: email,
+    },
+    to: process.env.EMAIL,
+    subject: 'Contact form.',
+    text: msg,
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail',
+      });
+    } else {
+      res.json({
+        status: 'success',
+      });
+    }
+  });
+};
+
+module.exports = { contact };
