f0b72566d4d2ddbc7c7b5e4b5ab3477dce2ec087
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-01 10:40:37 +0100

    Refactor

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -5,7 +5,7 @@ import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import { Container } from '@mui/system';
+// import { Container } from '@mui/system';
 import Avatar from '@mui/material/Avatar';
 import ListItemText from '@mui/material/ListItemText';
 import List from '@mui/material/List';
@@ -331,109 +331,105 @@ const UserProfile = () => {
   // );
 
   return (
-    <Container maxWidth='lg'>
-      <Box my={3}>
-        <Card>
-          <Grid container>
-            <Grid item xs={12} sm={4} p={2}>
-              <Box align={'center'}>
-                <Avatar
-                  alt='User Avatar'
-                  src={image}
-                  sx={{
-                    height: {
-                      xs: 120,
-                      md: 160,
-                    },
-                    width: {
-                      xs: 120,
-                      md: 160,
-                    },
-                  }}
-                />
-              </Box>
-              <Box my={2} px={2} align={'center'}>
-                <Typography variant='h6' component='div'>
-                  {user?.displayName}
-                </Typography>
-                <Typography
-                  variant={'subtitle2'}
-                  component='div'
-                  color={'textSecondary'}>
-                  {role ? 'Admin profile' : 'User profile'}
-                </Typography>
-              </Box>
-              <Divider />
-              <Box mt={2}>
-                <List disablePadding>
-                  <ListItem disablePadding>
-                    <ListItemButton
-                      selected={selectedIndex === 0}
-                      onClick={() => handleListItemClick(0)}>
-                      <ListItemIcon>
-                        <PersonIcon />
-                      </ListItemIcon>
-                      <ListItemText primary='About' />
-                    </ListItemButton>
-                  </ListItem>
-                  <ListItem disablePadding>
-                    <ListItemButton component='label' onChange={handleUpload}>
-                      <ListItemIcon>
-                        <PhotoCameraIcon />
-                      </ListItemIcon>
-                      <ListItemText primary='Change Avatar' />
-                      <input hidden accept='image/*' type='file' />
-                    </ListItemButton>
-                  </ListItem>
-                  {!role && (
-                    <ListItem disablePadding>
-                      <ListItemButton
-                        selected={selectedIndex === 1}
-                        onClick={() => handleListItemClick(1)}>
-                        <ListItemIcon>
-                          <DeleteIcon />
-                        </ListItemIcon>
-                        <ListItemText primary='Delete account' />
-                      </ListItemButton>
-                    </ListItem>
-                  )}
-                  {role && (
-                    <ListItem disablePadding>
-                      <ListItemButton component={NavLink} to='/dashboard'>
-                        <ListItemIcon>
-                          <DashboardIcon />
-                        </ListItemIcon>
-                        <ListItemText primary='Admin dashboard' />
-                      </ListItemButton>
-                    </ListItem>
-                  )}
-                  <ListItem disablePadding>
-                    <ListItemButton onClick={handleSignOut}>
-                      <ListItemIcon>
-                        <LogoutIcon />
-                      </ListItemIcon>
-                      <ListItemText primary='Logout' />
-                    </ListItemButton>
-                  </ListItem>
-                </List>
-              </Box>
-            </Grid>
-            <Grid item xs={12} sm={8} p={2}>
-              <Box
-                borderLeft={1}
-                px={4}
-                borderColor={'#0000001f'}
-                sx={{ borderWidth: 'thin', height: '100%' }}>
-                {selectedIndex === 0 && <AboutSection user={user} />}
-                {selectedIndex === 1 && (
-                  <DeleteAccountSection userImagesRef={userImagesRef} />
-                )}
-              </Box>
-            </Grid>
-          </Grid>
-        </Card>
-      </Box>
-    </Container>
+    <Card>
+      <Grid container>
+        <Grid item xs={12} sm={4} p={2}>
+          <Box align={'center'}>
+            <Avatar
+              alt='User Avatar'
+              src={image}
+              sx={{
+                height: {
+                  xs: 120,
+                  md: 160,
+                },
+                width: {
+                  xs: 120,
+                  md: 160,
+                },
+              }}
+            />
+          </Box>
+          <Box my={2} px={2} align={'center'}>
+            <Typography variant='h6' component='div'>
+              {user?.displayName}
+            </Typography>
+            <Typography
+              variant={'subtitle2'}
+              component='div'
+              color={'textSecondary'}>
+              {role ? 'Admin profile' : 'User profile'}
+            </Typography>
+          </Box>
+          <Divider />
+          <Box mt={2}>
+            <List disablePadding>
+              <ListItem disablePadding>
+                <ListItemButton
+                  selected={selectedIndex === 0}
+                  onClick={() => handleListItemClick(0)}>
+                  <ListItemIcon>
+                    <PersonIcon />
+                  </ListItemIcon>
+                  <ListItemText primary='About' />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component='label' onChange={handleUpload}>
+                  <ListItemIcon>
+                    <PhotoCameraIcon />
+                  </ListItemIcon>
+                  <ListItemText primary='Change Avatar' />
+                  <input hidden accept='image/*' type='file' />
+                </ListItemButton>
+              </ListItem>
+              {!role && (
+                <ListItem disablePadding>
+                  <ListItemButton
+                    selected={selectedIndex === 1}
+                    onClick={() => handleListItemClick(1)}>
+                    <ListItemIcon>
+                      <DeleteIcon />
+                    </ListItemIcon>
+                    <ListItemText primary='Delete account' />
+                  </ListItemButton>
+                </ListItem>
+              )}
+              {role && (
+                <ListItem disablePadding>
+                  <ListItemButton component={NavLink} to='/dashboard'>
+                    <ListItemIcon>
+                      <DashboardIcon />
+                    </ListItemIcon>
+                    <ListItemText primary='Admin dashboard' />
+                  </ListItemButton>
+                </ListItem>
+              )}
+              <ListItem disablePadding>
+                <ListItemButton onClick={handleSignOut}>
+                  <ListItemIcon>
+                    <LogoutIcon />
+                  </ListItemIcon>
+                  <ListItemText primary='Logout' />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={8} p={2}>
+          <Box
+            borderLeft={1}
+            px={4}
+            borderColor={'#0000001f'}
+            sx={{ borderWidth: 'thin', height: '100%' }}>
+            {selectedIndex === 0 && <AboutSection />}
+            {selectedIndex === 1 && (
+              <DeleteAccountSection userImagesRef={userImagesRef} />
+            )}
+          </Box>
+        </Grid>
+      </Grid>
+    </Card>
   );
 };
 
