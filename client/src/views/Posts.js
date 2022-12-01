73e6bc0481dc8a4867e7cb8e0a31d62ae8f58637
HiImWhite <wojttasek1999@gmail.com>
2022-12-02 00:15:52 +0100

    Update Posts.js

---
+++ client/src/views/Posts.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import { Box } from '@mui/material';
+import Sidebar from '../components/Sidebar/Sidebar';
+import Slider from '../components/Slider/Slider';
+import RecentPost from '../components/RecentPost/RecentPost';
+import FeaturedPost from '../components/FeaturedPost/FeaturedPost';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+const Posts = () => {
+  const [page, setPage] = React.useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  const featuredPosts = [
+    {
+      title: 'Featured post',
+      date: 'Nov 12',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random',
+      imageLabel: 'Image Text',
+    },
+    {
+      title: 'Post title',
+      date: 'Nov 11',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random',
+      imageLabel: 'Image Text',
+    },
+    {
+      title: 'Featurede poste',
+      date: 'Nov 12',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random',
+      imageLabel: 'Image Text',
+    },
+    {
+      title: 'Poste titlee',
+      date: 'Nov 11',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random',
+      imageLabel: 'Image Text',
+    },
+  ];
+
+  return (
+    <Container maxWidth='lg'>
+      <Grid container spacing={6} sx={{ mt: 1 }}>
+        {featuredPosts.map((post) => (
+          <FeaturedPost key={post.title} post={post} />
+        ))}
+      </Grid>
+      <Box margin={3} display={'flex'} justifyContent={'center'}>
+        <Stack spacing={2}>
+          <Pagination count={10} page={page} onChange={handleChange} />
+        </Stack>
+      </Box>
+    </Container>
+  );
+};
+
+export default Posts;
