bbb803766b9e40eef845aaf8488ac0daae0e65ab
HiImWhite <wojttasek1999@gmail.com>
2022-12-02 14:08:02 +0100

    Post counter

---
+++ client/src/views/admin/PostNumber.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import Title from './Title.js';
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+const Deposits = () => {
+  return (
+    <>
+      <Title>Number of posts</Title>
+      <Typography component='p' variant='h4'>
+        420
+      </Typography>
+      <Typography color='text.secondary' sx={{ flex: 1 }}>
+        on 15 March, 2019
+      </Typography>
+    </>
+  );
+};
+
+export default Deposits;
