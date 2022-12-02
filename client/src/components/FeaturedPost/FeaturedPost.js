48d9f217a0168abddc619a4bab72bf781f20535f
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-02 12:01:26 +0100

    Added navlink

--- client/src/components/FeaturedPost/FeaturedPost.js
+++ client/src/components/FeaturedPost/FeaturedPost.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
@@ -12,7 +13,7 @@ const FeaturedPost = (props) => {
 
   return (
     <Grid item xs={12} md={6}>
-      <CardActionArea component='a' href='#'>
+      <CardActionArea component={NavLink} to={post.title}>
         <Card sx={{ display: 'flex' }}>
           <CardContent sx={{ flex: 1 }}>
             <Typography component='h2' variant='h5'>
