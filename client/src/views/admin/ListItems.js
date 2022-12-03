ee7f3909a743459f47b983081e45e61c84b3db9a
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-03 18:17:25 +0100

    Added HomeIcon

--- client/src/views/admin/ListItems.js
+++ client/src/views/admin/ListItems.js
@@ -9,6 +9,7 @@ import PeopleIcon from '@mui/icons-material/People';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import LayersIcon from '@mui/icons-material/Layers';
 import AssignmentIcon from '@mui/icons-material/Assignment';
+import HomeIcon from '@mui/icons-material/Home';
 
 export const mainListItems = (
   <>
@@ -65,6 +66,12 @@ export const secondaryListItems = (
     <ListItemButton>
       <ListItemIcon>
         <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary='Year-end sale' />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <HomeIcon />
       </ListItemIcon>
       <ListItemText primary='Year-end sale' />
     </ListItemButton>
