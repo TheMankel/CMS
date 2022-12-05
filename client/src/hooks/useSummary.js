c56ef6a3d4ee885d7c22008c56fd28474561843d
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-05 19:06:40 +0100

    Added custom useSummary hook

---
+++ client/src/hooks/useSummary.js
@@ -0,0 +1,25 @@
+import { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+
+const useSummary = () => {
+  const [summary, setSummary] = useState({});
+
+  const getSummary = useCallback(async () => {
+    try {
+      const { data } = await axios.get('http://localhost:8000/api/summary');
+      console.log(data);
+
+      setSummary(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    getSummary();
+  }, [getSummary]);
+
+  return { summary };
+};
+
+export default useSummary;
