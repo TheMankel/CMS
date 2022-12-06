4fa2acc9848cd11e3a5bf53ca5b8b44883908338
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-06 11:38:28 +0100

    Update Dashboard.js

--- client/src/views/admin/Dashboard.js
+++ client/src/views/admin/Dashboard.js
@@ -25,199 +25,247 @@ import PeopleIcon from '@mui/icons-material/People';
 import ListItems from './ListItems.js';
 import useSummary from '../../hooks/useSummary.js';
 
-const drawerWidth = 240;
+// const drawerWidth = 240;
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
+// const AppBar = styled(MuiAppBar, {
+//   shouldForwardProp: (prop) => prop !== 'open',
+// })(({ theme, open }) => ({
+//   zIndex: theme.zIndex.drawer + 1,
+//   transition: theme.transitions.create(['width', 'margin'], {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.leavingScreen,
+//   }),
+//   ...(open && {
+//     marginLeft: drawerWidth,
+//     width: `calc(100% - ${drawerWidth}px)`,
+//     transition: theme.transitions.create(['width', 'margin'], {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//   }),
+// }));
 
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  '& .MuiDrawer-paper': {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    boxSizing: 'border-box',
-    ...(!open && {
-      overflowX: 'hidden',
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      width: theme.spacing(7),
-      [theme.breakpoints.up('sm')]: {
-        width: theme.spacing(9),
-      },
-    }),
-  },
-}));
+// const Drawer = styled(MuiDrawer, {
+//   shouldForwardProp: (prop) => prop !== 'open',
+// })(({ theme, open }) => ({
+//   '& .MuiDrawer-paper': {
+//     position: 'relative',
+//     whiteSpace: 'nowrap',
+//     width: drawerWidth,
+//     transition: theme.transitions.create('width', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//     boxSizing: 'border-box',
+//     ...(!open && {
+//       overflowX: 'hidden',
+//       transition: theme.transitions.create('width', {
+//         easing: theme.transitions.easing.sharp,
+//         duration: theme.transitions.duration.leavingScreen,
+//       }),
+//       width: theme.spacing(7),
+//       [theme.breakpoints.up('sm')]: {
+//         width: theme.spacing(9),
+//       },
+//     }),
+//   },
+// }));
 
 const Dashboard = () => {
   const { summary } = useSummary();
-  const [open, setOpen] = useState(true);
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
+  // const [open, setOpen] = useState(true);
+  // const toggleDrawer = () => {
+  //   setOpen(!open);
+  // };
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position='absolute' open={open}>
-        <Toolbar
-          sx={{
-            pr: '24px', // keep right padding when drawer closed
-          }}>
-          <IconButton
-            edge='start'
-            color='inherit'
-            aria-label='open drawer'
-            onClick={toggleDrawer}
-            sx={{
-              marginRight: '36px',
-              ...(open && { display: 'none' }),
-            }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            component='h1'
-            variant='h6'
-            color='inherit'
-            noWrap
-            sx={{ flexGrow: 1 }}>
-            Dashboard
-          </Typography>
-          <IconButton color='inherit'>
-            <Badge badgeContent={4} color='secondary'>
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant='permanent' open={open}>
-        <Toolbar
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            px: [1],
-          }}>
-          <IconButton onClick={toggleDrawer}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </Toolbar>
-        <Divider />
-        <List
-          component='nav'
-          sx={{
-            position: 'relative',
-            overflow: 'auto',
-            overflowX: 'hidden',
-            maxHeight: 'calc(100vh - 4em)',
-          }}>
-          {/* {mainListItems}
-          <Divider sx={{ my: 1 }} />
-          {secondaryListItems}
-          <Divider sx={{ my: 1 }} />
-          {otherListItems} */}
-          <ListItems open={open} />
-        </List>
-      </Drawer>
-      <Box
-        component='main'
-        sx={{
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
-              ? theme.palette.grey[100]
-              : theme.palette.grey[900],
-          flexGrow: 1,
-          height: '100vh',
-          overflow: 'auto',
-        }}>
-        <Toolbar />
-        <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
-          <Grid container spacing={3}>
-            {/* PostNumber */}
-            <Grid item xs={12} md={4} lg={4}>
-              {/* <Paper
-                sx={{
-                  p: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: 240,
-                }}>
-                <PostNumber />
-              </Paper> */}
-              <WidgetSummary
-                icon={<ArticleIcon sx={{ color: '#ffffff' }} />}
-                title='Posts'
-                value={summary.posts}
-                color='#c70046'
-              />
-            </Grid>
-            {/* Recent AccountNumber */}
-            <Grid item xs={12} md={4} lg={4}>
-              {/* <Paper
-                sx={{
-                  p: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: 240,
-                }}>
-                <AccountNumber />
-              </Paper> */}
-              <WidgetSummary
-                icon={<CategoryIcon sx={{ color: '#ffffff' }} />}
-                title='Categories'
-                value={summary.categories}
-                color='#00c788'
-              />
-            </Grid>
-            {/* LOGO */}
-            <Grid item xs={12} md={4} lg={4}>
-              {/* <Paper
-                sx={{
-                  p: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: 240,
-                }}>
-                <Logo />
-              </Paper> */}
-              <WidgetSummary
-                icon={<PeopleIcon sx={{ color: '#ffffff' }} />}
-                title='Users'
-                value={summary.users}
-                color='#ffbf00'
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                <RecentUsers />
-              </Paper>
-            </Grid>
+    // <Box sx={{ display: 'flex' }}>
+    //   <CssBaseline />
+    //   <AppBar position='absolute' open={open}>
+    //     <Toolbar
+    //       sx={{
+    //         pr: '24px', // keep right padding when drawer closed
+    //       }}>
+    //       <IconButton
+    //         edge='start'
+    //         color='inherit'
+    //         aria-label='open drawer'
+    //         onClick={toggleDrawer}
+    //         sx={{
+    //           marginRight: '36px',
+    //           ...(open && { display: 'none' }),
+    //         }}>
+    //         <MenuIcon />
+    //       </IconButton>
+    //       <Typography
+    //         component='h1'
+    //         variant='h6'
+    //         color='inherit'
+    //         noWrap
+    //         sx={{ flexGrow: 1 }}>
+    //         Dashboard
+    //       </Typography>
+    //       <IconButton color='inherit'>
+    //         <Badge badgeContent={4} color='secondary'>
+    //           <NotificationsIcon />
+    //         </Badge>
+    //       </IconButton>
+    //     </Toolbar>
+    //   </AppBar>
+    //   <Drawer variant='permanent' open={open}>
+    //     <Toolbar
+    //       sx={{
+    //         display: 'flex',
+    //         alignItems: 'center',
+    //         justifyContent: 'flex-end',
+    //         px: [1],
+    //       }}>
+    //       <IconButton onClick={toggleDrawer}>
+    //         <ChevronLeftIcon />
+    //       </IconButton>
+    //     </Toolbar>
+    //     <Divider />
+    //     <List
+    //       component='nav'
+    //       sx={{
+    //         position: 'relative',
+    //         overflow: 'auto',
+    //         overflowX: 'hidden',
+    //         maxHeight: 'calc(100vh - 4em)',
+    //       }}>
+    //       {/* {mainListItems}
+    //       <Divider sx={{ my: 1 }} />
+    //       {secondaryListItems}
+    //       <Divider sx={{ my: 1 }} />
+    //       {otherListItems} */}
+    //       <ListItems open={open} />
+    //     </List>
+    //   </Drawer>
+    //   <Box
+    //     component='main'
+    //     sx={{
+    //       backgroundColor: (theme) =>
+    //         theme.palette.mode === 'light'
+    //           ? theme.palette.grey[100]
+    //           : theme.palette.grey[900],
+    //       flexGrow: 1,
+    //       height: '100vh',
+    //       overflow: 'auto',
+    //     }}>
+    //     <Toolbar />
+    //     <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
+    //       <Grid container spacing={3}>
+    //         {/* PostNumber */}
+    //         <Grid item xs={12} md={4} lg={4}>
+    //           {/* <Paper
+    //             sx={{
+    //               p: 2,
+    //               display: 'flex',
+    //               flexDirection: 'column',
+    //               height: 240,
+    //             }}>
+    //             <PostNumber />
+    //           </Paper> */}
+    //           <WidgetSummary
+    //             icon={<ArticleIcon sx={{ color: '#ffffff' }} />}
+    //             title='Posts'
+    //             value={summary.posts}
+    //             color='#c70046'
+    //           />
+    //         </Grid>
+    //         {/* Recent AccountNumber */}
+    //         <Grid item xs={12} md={4} lg={4}>
+    //           {/* <Paper
+    //             sx={{
+    //               p: 2,
+    //               display: 'flex',
+    //               flexDirection: 'column',
+    //               height: 240,
+    //             }}>
+    //             <AccountNumber />
+    //           </Paper> */}
+    //           <WidgetSummary
+    //             icon={<CategoryIcon sx={{ color: '#ffffff' }} />}
+    //             title='Categories'
+    //             value={summary.categories}
+    //             color='#00c788'
+    //           />
+    //         </Grid>
+    //         {/* LOGO */}
+    //         <Grid item xs={12} md={4} lg={4}>
+    //           {/* <Paper
+    //             sx={{
+    //               p: 2,
+    //               display: 'flex',
+    //               flexDirection: 'column',
+    //               height: 240,
+    //             }}>
+    //             <Logo />
+    //           </Paper> */}
+    //           <WidgetSummary
+    //             icon={<PeopleIcon sx={{ color: '#ffffff' }} />}
+    //             title='Users'
+    //             value={summary.users}
+    //             color='#ffbf00'
+    //           />
+    //         </Grid>
+    //         <Grid item xs={12}>
+    //           <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+    //             <RecentUsers />
+    //           </Paper>
+    //         </Grid>
+    //       </Grid>
+    //       <Copyright sx={{ pt: 4 }} />
+    //     </Container>
+    //   </Box>
+    // </Box>
+
+    <Box
+      component='main'
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light'
+            ? theme.palette.grey[100]
+            : theme.palette.grey[900],
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+      }}>
+      <Toolbar />
+      <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={4} lg={4}>
+            <WidgetSummary
+              icon={<ArticleIcon sx={{ color: '#ffffff' }} />}
+              title='Posts'
+              value={summary.posts}
+              color='#c70046'
+            />
           </Grid>
-          <Copyright sx={{ pt: 4 }} />
-        </Container>
-      </Box>
+          <Grid item xs={12} md={4} lg={4}>
+            <WidgetSummary
+              icon={<CategoryIcon sx={{ color: '#ffffff' }} />}
+              title='Categories'
+              value={summary.categories}
+              color='#00c788'
+            />
+          </Grid>
+          <Grid item xs={12} md={4} lg={4}>
+            <WidgetSummary
+              icon={<PeopleIcon sx={{ color: '#ffffff' }} />}
+              title='Users'
+              value={summary.users}
+              color='#ffbf00'
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+              <RecentUsers />
+            </Paper>
+          </Grid>
+        </Grid>
+        <Copyright sx={{ pt: 4 }} />
+      </Container>
     </Box>
   );
 };
