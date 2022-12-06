099f52a9ba396eba66a433535e126a02198d2dee
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-06 14:24:26 +0100

    Added dynamic data

--- client/src/views/admin/RecentUsers.js
+++ client/src/views/admin/RecentUsers.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Link from '@mui/material/Link';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,40 +7,38 @@ import TableRow from '@mui/material/TableRow';
 import Title from './Title';
 
 // Generate Order Data
-function createData(id, date, userName) {
-  return { id, date, userName };
-}
+// function createData(id, date, userName) {
+//   return { id, date, userName };
+// }
 
-const rows = [
-  createData(0, '16 Mar, 2019', 'Elvis Presley'),
-  createData(1, '16 Mar, 2019', 'Paul McCartney'),
-  createData(2, '16 Mar, 2019', 'Tom Scholz'),
-  createData(3, '16 Mar, 2019', 'Michael Jackson'),
-  createData(4, '15 Mar, 2019', 'Bruce Springsteen'),
-];
+// const rows = [
+//   createData(0, '16 Mar, 2019', 'Elvis Presley'),
+//   createData(1, '16 Mar, 2019', 'Paul McCartney'),
+//   createData(2, '16 Mar, 2019', 'Tom Scholz'),
+//   createData(3, '16 Mar, 2019', 'Michael Jackson'),
+//   createData(4, '15 Mar, 2019', 'Bruce Springsteen'),
+// ];
 
-function preventDefault(e) {
-  e.preventDefault();
-}
+const RecentUsers = (props) => {
+  const { recentUsers } = props;
 
-const RecentUsers = () => {
   return (
     <>
       <Title>Recent users</Title>
       <Table size='small'>
         <TableHead>
           <TableRow>
-            <TableCell>id</TableCell>
-            <TableCell>Name</TableCell>
+            <TableCell>lp</TableCell>
+            <TableCell>Full name</TableCell>
             <TableCell>Sign up date</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.id}</TableCell>
-              <TableCell>{row.userName}</TableCell>
-              <TableCell>{row.date}</TableCell>
+          {recentUsers.map((user, i) => (
+            <TableRow key={i}>
+              <TableCell>{i}</TableCell>
+              <TableCell>{user?.fullName}</TableCell>
+              <TableCell>{user?.created}</TableCell>
             </TableRow>
           ))}
         </TableBody>
