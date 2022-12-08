6a665f343ec40f15c9a3e0298a1d96210ca370f9
HiImWhite <wojttasek1999@gmail.com>
2022-12-08 10:00:18 +0100

    Update ManagePosts.js

--- client/src/views/admin/ManagePosts.js
+++ client/src/views/admin/ManagePosts.js
@@ -8,28 +8,43 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Copyright from '../../components/Copyright/Copyright.js';
 import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import Title from './Title';
 
 const ManagePosts = () => {
   const [value, setValue] = useState('');
 
-  const modules = {
-    toolbar: [
-      // [{ font: [] }],
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      ['bold', 'italic', 'underline', 'strike'],
-      [{ color: [] }, { background: [] }],
-      [{ script: 'sub' }, { script: 'super' }],
-      ['blockquote', 'code-block'],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ indent: '-1' }, { indent: '+1' }, { align: [] }],
-      ['link', 'image', 'video'],
-      ['clean'],
-    ],
-  };
-
   const handleNewPost = () => {
     console.log(value);
   };
+
+  const handleDeletePost = () => {
+    console.log(value);
+  };
+
+  const handleEditPost = () => {
+    console.log(value);
+  };
+
+  // Generate Order Data
+  function createData(id, created, postTitle) {
+    return { id, created, postTitle };
+  }
+
+  const rows = [
+    createData(0, '16 Mar, 2019', 'Post test'),
+    createData(1, '16 Mar, 2019', 'Post 1'),
+    createData(2, '16 Mar, 2019', 'Post 2'),
+    createData(3, '16 Mar, 2019', 'Post 3'),
+    createData(4, '15 Mar, 2019', 'Post 4'),
+  ];
 
   return (
     <Box
@@ -59,12 +74,50 @@ const ManagePosts = () => {
                 value={value}
                 onChange={setValue}
                 placeholder='Write something'
-                modules={modules}
               />
             </Paper>
           </Grid>
           <Grid item xs={12}>
             <Button onClick={handleNewPost}>Add new post</Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid item xs={12}>
+              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                <Table size='small'>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>id</TableCell>
+                      <TableCell>Post title</TableCell>
+                      <TableCell>Created</TableCell>
+                      <TableCell align='center'>Actions</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((user, i) => (
+                      <TableRow key={i}>
+                        <TableCell>{i}</TableCell>
+                        <TableCell>{user?.postTitle}</TableCell>
+                        <TableCell>{user?.created}</TableCell>
+                        <TableCell align='center'>
+                          <IconButton
+                            onClick={handleEditPost}
+                            aria-label='edit post'
+                            component='label'>
+                            <EditIcon />
+                          </IconButton>
+                          <IconButton
+                            onClick={handleDeletePost}
+                            aria-label='delete post'
+                            component='label'>
+                            <DeleteIcon />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
+            </Grid>
           </Grid>
         </Grid>
         <Copyright sx={{ pt: 4 }} />
