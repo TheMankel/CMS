16a0edce8734e82207382a70b9e5743185012aaf
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-09 15:55:26 +0100

    Added comments component

---
+++ client/src/components/Comments/Comments.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+
+const Comments = (props) => {
+  const { comments, fullName, commentText, avatar } = props;
+
+  return (
+    <List>
+      {comments.map((comment, i) => (
+        <Box key={i}>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar alt={comment.fullName} src={comment.avatar} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={comment.fullName}
+              secondary={comment.commentText}
+            />
+          </ListItem>
+          {comments.length !== i + 1 && <Divider />}
+        </Box>
+      ))}
+    </List>
+  );
+};
+
+export default Comments;
