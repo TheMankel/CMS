9cfb53763e8de1a9b702036677a11ec8c08c80d5
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-14 20:30:11 +0100

    Added allPosts and postDetails functions

--- server/controllers/publicController.js
+++ server/controllers/publicController.js
@@ -1,6 +1,7 @@
 const { db } = require('../config/firebase-config');
 
 const blogCollectionRef = db.collection('cms');
+const postsCollectionRef = db.collection('posts');
 
 const navigation = async (req, res, next) => {
   try {
@@ -94,8 +95,44 @@ const slider = async (req, res, next) => {
   }
 };
 
+const allPosts = async (req, res, next) => {
+  try {
+    const postsRef = await postsCollectionRef.orderBy('date', 'asc').get();
+
+    const data = {
+      posts: [],
+    };
+
+    postsRef.forEach((post) => {
+      data.posts.push(post.data());
+    });
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+};
+
+const postDetails = async (req, res, next) => {
+  try {
+    const { id } = req.body;
+
+    const postsRef = await postsCollectionRef?.doc(id).get();
+
+    const data = postsRef.data();
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+};
+
 module.exports = {
   navigation,
   about,
   slider,
+  allPosts,
+  postDetails,
 };
