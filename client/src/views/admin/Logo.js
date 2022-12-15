cae65b0e171a5b3918379509f23776b4058dc903
HiImWhite <wojttasek1999@gmail.com>
2022-12-15 22:28:01 +0100

    Create Logo.js

---
+++ client/src/views/admin/Logo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+
+const Logo = () => {
+  return (
+    <Box
+      component='main'
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light'
+            ? theme.palette.grey[100]
+            : theme.palette.grey[900],
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+      }}>
+      <Toolbar />
+      <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}></Container>
+    </Box>
+  );
+};
+
+export default Logo;
