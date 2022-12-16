47b93e1179461bf4613c33ad584c729bac1d6cc5
HiImWhite <wojttasek1999@gmail.com>
2022-12-16 17:17:18 +0100

    Update Logo.js

--- client/src/views/admin/Logo.js
+++ client/src/views/admin/Logo.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Title from './Title';
+import axios from 'axios';
+import { createRef, uploadImage, downloadImage } from '../../lib/storage';
 
 const Logo = () => {
+  const [blogName, setBlogName] = useState('');
+  const [logo, setLogo] = useState(null);
+
+  const handleUpload = async (e) => {
+    try {
+      const logoFile = e.target.files[0];
+      if (!logoFile) return;
+      console.log(logoFile);
+      setLogo(logoFile);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleCancel = () => {
+    setBlogName('');
+    setLogo('none');
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+
+    if (!logo && !blogName) return;
+
+    try {
+      const title = blogName?.toLowerCase().replace(' ', '-');
+      const blogLogoRef = createRef(`logoImages/${title}`);
+
+      await uploadImage(blogLogoRef, logo);
+      const logoUrl = await downloadImage(blogLogoRef);
+
+      const data = {
+        title: blogName,
+        logo: logoUrl,
+      };
+
+      await axios.post('http://localhost:8000/api/new-name-logo', data, {
+        withCredentials: true,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    setBlogName('');
+    setLogo('none');
+  };
+
   return (
     <Box
       component='main'
@@ -17,7 +71,68 @@ const Logo = () => {
         overflow: 'auto',
       }}>
       <Toolbar />
-      <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}></Container>
+      <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+              <Box>
+                <Title>Blog name</Title>
+                <TextField
+                  id='set-blogName'
+                  label='Write new blog name'
+                  variant='outlined'
+                  value={blogName}
+                  onChange={(e) => setBlogName(e.target.value)}
+                  fullWidth
+                  sx={{ mt: 1 }}
+                />
+              </Box>
+              <Box display='flex' flexDirection='column' sx={{ mt: 2 }}>
+                <Title>Logo</Title>
+                <Button
+                  id='upload-logo'
+                  variant='contained'
+                  component='label'
+                  onChange={handleUpload}
+                  sx={{
+                    alignSelf: 'center',
+                    textTransform: 'none',
+                  }}>
+                  Upload
+                  <input hidden accept='image/*' type='file' />
+                </Button>
+                <Box display='flex' gap={1} sx={{ mt: 1 }}>
+                  <Typography>Currently uploaded logo:</Typography>
+                  <Typography color='GrayText'>
+                    {logo?.name ? logo.name : 'none'}
+                  </Typography>
+                </Box>
+              </Box>
+              <Box alignSelf='center' sx={{ mt: 5 }}>
+                <Button
+                  variant='outlined'
+                  onClick={handleCancel}
+                  sx={{
+                    mx: '4px',
+                    textTransform: 'none',
+                  }}>
+                  Cancel
+                </Button>
+                <Button
+                  variant='contained'
+                  type='submit'
+                  onClick={handleUpdate}
+                  sx={{
+                    mx: '4px',
+                    textTransform: 'none',
+                  }}>
+                  Update
+                </Button>
+              </Box>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   );
 };
