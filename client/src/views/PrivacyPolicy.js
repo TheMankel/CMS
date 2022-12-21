36b814ddc94230414dd8805d47d9775fbf77c983
HiImWhite <wojttasek1999@gmail.com>
2022-12-21 21:11:06 +0100

    Create PrivacyPolicy.js

---
+++ client/src/views/PrivacyPolicy.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import ContactCard from '../components/ContactCard/ContactCard';
+import Content from '../components/Content/Content';
+
+const PrivacyPolicy = () => {
+  const theme = useTheme();
+
+  return (
+    <Box>
+      <Container>
+        <Box boxShadow={4} borderRadius={2} mt={5} mb={5}>
+          <Box bgcolor={theme.palette.primary.main} borderRadius={2}>
+            <Container paddingX={{ xs: 2, sm: 4 }}>
+              <Typography
+                variant={'h3'}
+                gutterBottom
+                sx={{
+                  fontWeight: 700,
+                  color: theme.palette.common.white,
+                }}>
+                Privacy Policy
+              </Typography>
+              <Typography
+                gutterBottom
+                sx={{
+                  color: theme.palette.common.white,
+                }}>
+                Last modified on <strong>23 Feb, 2021</strong>
+              </Typography>
+            </Container>
+            <Box
+              component={'svg'}
+              preserveAspectRatio='none'
+              xmlns='http://www.w3.org/2000/svg'
+              x='0px'
+              y='0px'
+              viewBox='0 0 1920 100.1'
+              width={'100%'}
+              marginBottom={-1}>
+              <path
+                fill={theme.palette.background.paper}
+                d='M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z'></path>
+            </Box>
+          </Box>
+          <Container
+            paddingTop={'0 !important'}
+            paddingX={{ xs: 2, sm: 4 }}
+            position={'relative'}
+            top={0}>
+            <Box
+              component={Grid}
+              container
+              spacing={4}
+              flexDirection={{ xs: 'column-reverse', md: 'row' }}>
+              <Grid item xs={12} md={9}>
+                <Content />
+              </Grid>
+              <Grid item xs={12} md={3}>
+                <Box
+                  position={'sticky'}
+                  top={theme.spacing(10)}
+                  className={'sticky'}>
+                  <ContactCard />
+                </Box>
+              </Grid>
+            </Box>
+          </Container>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default PrivacyPolicy;
