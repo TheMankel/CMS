402dd002c644c4bf8b1d8d99a509f59f82f6c7b3
HiImWhite <wojttasek1999@gmail.com>
2022-12-22 18:05:41 +0100

    Update ManagePrivacyPolicy.js

--- client/src/views/admin/ManagePrivacyPolicy.js
+++ client/src/views/admin/ManagePrivacyPolicy.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Title from './Title';
+import axios from 'axios';
 
 const PrivacyPolicy = () => {
+  const [content, setContent] = useState('');
+
+  const handleCancel = () => {
+    setContent('');
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+
+    if (!content) return;
+
+    try {
+      const data = {
+        content: content,
+      };
+
+      await axios.post('http://localhost:8000/api/update-policy', data, {
+        withCredentials: true,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    setContent('');
+  };
+
   return (
     <Box
       component='main'
@@ -17,7 +49,48 @@ const PrivacyPolicy = () => {
         overflow: 'auto',
       }}>
       <Toolbar />
-      <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}></Container>
+      <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper
+              sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: 1 }}>
+              <Box>
+                <Title>Edit Privacy Policy</Title>
+              </Box>
+              <ReactQuill
+                theme='snow'
+                value={content}
+                onChange={setContent}
+                placeholder='Write something'
+              />
+              <Box alignSelf='center'>
+                <Button
+                  variant='outlined'
+                  onClick={handleCancel}
+                  sx={{
+                    mx: '4px',
+                    textTransform: 'none',
+                  }}>
+                  Cancel
+                </Button>
+                <Button
+                  variant='contained'
+                  type='submit'
+                  onClick={handleUpdate}
+                  sx={{
+                    mx: '4px',
+                    textTransform: 'none',
+                  }}>
+                  Update
+                </Button>
+              </Box>
+              <Box>
+                <Title>Preview the current privacy policy</Title>
+              </Box>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   );
 };
