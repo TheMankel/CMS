e2282db04fb63aca6ab25679197758348aed1a5d
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-23 15:33:37 +0100

    Added categories, refactored header and footer data

--- client/src/layouts/MainPublic.js
+++ client/src/layouts/MainPublic.js
@@ -8,20 +8,45 @@ import axios from 'axios';
 
 const MainPublic = (props) => {
   const { showDetailed } = props;
-  const [webTitle, setWebTitle] = useState('');
-  const [sections, setSections] = useState([{ title: '', url: '' }]);
-  const [categories, setCategories] = useState([{ title: '', links: [''] }]);
   const [logo, setLogo] = useState('');
+  const [webTitle, setWebTitle] = useState('');
+  const [contact, setContact] = useState({});
+  const [categories, setCategories] = useState(null);
+  // const [sections, setSections] = useState([{ title: '', url: '' }]);
+  // const [categories, setCategories] = useState([{ title: '', links: [''] }]);
   const theme = createTheme();
 
   const getData = async () => {
     try {
-      const data = await axios.get('http://localhost:8000/api/navigation');
+      // const data = await axios.get('http://localhost:8000/api/navigation');
+      const blogRes = await axios.get('http://localhost:8000/api/blog');
+      const contactRes = await axios.get('http://localhost:8000/api/contact');
+      const categoriesRes = await axios.get(
+        'http://localhost:8000/api/categories',
+      );
 
-      setCategories(data?.data?.categories);
-      setSections(data?.data?.sections);
-      setWebTitle(data?.data?.title);
-      setLogo(data?.data?.logo);
+      const blogData = blogRes?.data;
+      const contactData = contactRes?.data;
+      const categoriesData = categoriesRes?.data;
+
+      // setCategories(data?.data?.categories);
+      // setSections(data?.data?.sections);
+      // setWebTitle(data?.data?.title);
+      // setLogo(data?.data?.logo);
+
+      const categoriesMap = categoriesData.map((category) => ({
+        title: category.title,
+        url: `/posts/category/${category.title
+          .toLowerCase()
+          .replace(' ', '-')}`,
+      }));
+
+      console.log(categoriesMap);
+
+      setWebTitle(blogData?.title);
+      setLogo(blogData?.logo);
+      setContact(contactData);
+      setCategories(categoriesMap);
     } catch (err) {
       console.log(err);
     }
@@ -30,6 +55,79 @@ const MainPublic = (props) => {
   useEffect(() => {
     getData();
   }, []);
+
+  // const postCategories = [
+  //   {
+  //     title: 'Europe',
+  //     url: '/europe',
+  //   },
+  //   {
+  //     title: 'Asia',
+  //     url: '/asia',
+  //   },
+  //   {
+  //     title: 'North America',
+  //     url: '/north-america',
+  //   },
+  //   {
+  //     title: 'South America',
+  //     url: '/south-america',
+  //   },
+  //   {
+  //     title: 'Australia',
+  //     url: '/australia',
+  //   },
+  //   {
+  //     title: 'Africa',
+  //     url: '/africa',
+  //   },
+  // ];
+
+  const navHeader = [
+    {
+      title: 'Home',
+      url: '/',
+    },
+    {
+      title: 'Posts',
+      url: '/posts',
+    },
+    // ...postCategories,
+    // ...categories,
+    {
+      title: 'About',
+      url: '/about',
+    },
+    {
+      title: 'Contact',
+      url: '/contact',
+    },
+  ];
+
+  // navHeader.splice(2, 0, ...postCategories);
+  if (categories) navHeader?.splice(2, 0, ...categories);
+
+  const navFooter = [
+    {
+      title: 'Posts',
+      links: [
+        { title: 'All Posts', url: '/posts' },
+        { title: 'Categories', url: '/categories' },
+      ],
+    },
+    {
+      title: 'Blog',
+      links: [
+        { title: 'About', url: '/about' },
+        { title: 'Privacy Policy', url: '/privacy-policy' },
+      ],
+    },
+  ];
+
+  // const contact = {
+  //   email: 'blog@blog.com',
+  //   phone: '+48123456789',
+  // };
 
   // const sections = [
   //   { title: 'Technology', url: '#' },
@@ -68,13 +166,16 @@ const MainPublic = (props) => {
       <Header
         title={webTitle}
         logo={logo}
-        sections={sections}
+        // sections={sections}
+        sections={navHeader}
         showDetailed={showDetailed}
       />
       <Outlet />
       <Footer
         title={webTitle}
-        categories={categories}
+        // categories={categories}
+        categories={navFooter}
+        contact={contact}
         showDetailed={showDetailed}
       />
     </ThemeProvider>
