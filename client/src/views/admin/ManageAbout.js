2283e5e329b381b85aa24da37a41e8209193d0cb
HiImWhite <wojttasek1999@gmail.com>
2022-12-29 17:45:37 +0100

    Update ManageAbout.js

--- client/src/views/admin/ManageAbout.js
+++ client/src/views/admin/ManageAbout.js
@@ -1,10 +1,20 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import AboutTabPanel from './AboutTabPanel';
 
 const AboutAdmin = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <Box
       component='main'
@@ -18,7 +28,34 @@ const AboutAdmin = () => {
         overflow: 'auto',
       }}>
       <Toolbar />
-      <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}></Container>
+      <Container maxWidth='lg' sx={{ my: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper>
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  variant='fullWidth'
+                  aria-label='full width tabs about'>
+                  <Tab label='Story' />
+                  <Tab label='Manage Team' />
+                </Tabs>
+              </Box>
+            </Paper>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+              }}>
+              <AboutTabPanel value={value} index={0} />
+              <AboutTabPanel value={value} index={1} />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   );
 };
