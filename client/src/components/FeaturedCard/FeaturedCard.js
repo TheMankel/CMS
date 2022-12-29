900c4a028ea9b62b6a57a80167a9c78614572801
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-29 15:25:58 +0100

    Added skeleton to FeaturedCard

--- client/src/components/FeaturedCard/FeaturedCard.js
+++ client/src/components/FeaturedCard/FeaturedCard.js
@@ -7,9 +7,10 @@ import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Skeleton from '@mui/material/Skeleton';
 
 const FeaturedCard = (props) => {
-  const { item, url = '/', text = '' } = props;
+  const { item, url = '/', text = '', isLoading } = props;
 
   return (
     <Grid item xs={12} md={6}>
@@ -18,28 +19,52 @@ const FeaturedCard = (props) => {
         to={url + item.title?.toLowerCase().replace(' ', '-')}>
         <Card sx={{ display: 'flex' }}>
           <CardContent sx={{ flex: 1 }}>
-            <Typography component='h2' variant='h5'>
-              {item.title}
-            </Typography>
-            <Typography
-              component='p'
-              variant='subtitle1'
-              color='text.secondary'>
-              {item.date}
-            </Typography>
-            <Typography variant='subtitle1' paragraph>
-              {item.description}
-            </Typography>
-            <Typography component='h3' variant='subtitle1' color='primary'>
-              {text}
-            </Typography>
+            {isLoading && (
+              <>
+                <Skeleton variant='text' width='50%' />
+                <Skeleton variant='text' width='25%' />
+                <Skeleton variant='text' width='75%' />
+                <Skeleton variant='text' width='50%' />
+              </>
+            )}
+            {!isLoading && (
+              <>
+                <Typography component='h2' variant='h5'>
+                  {item.title}
+                </Typography>
+                <Typography
+                  component='p'
+                  variant='subtitle1'
+                  color='text.secondary'>
+                  {item.date}
+                </Typography>
+                <Typography variant='subtitle1' paragraph>
+                  {item.description}
+                </Typography>
+                <Typography component='h3' variant='subtitle1' color='primary'>
+                  {text}
+                </Typography>
+              </>
+            )}
           </CardContent>
-          <CardMedia
-            component='img'
-            image={item.image || 'https://source.unsplash.com/random'}
-            alt={item.title}
-            sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
-          />
+          {isLoading && (
+            <CardMedia>
+              <Skeleton
+                variant='rounded'
+                width={160}
+                height={160}
+                sx={{ display: { xs: 'none', sm: 'block' } }}
+              />
+            </CardMedia>
+          )}
+          {!isLoading && (
+            <CardMedia
+              component='img'
+              image={item.image || 'https://source.unsplash.com/random'}
+              alt={item.title}
+              sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
+            />
+          )}
         </Card>
       </CardActionArea>
     </Grid>
