91828f233020eb56398c3198897bbbc92b0a0b57
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-12-30 09:44:58 +0100

    Added isLoading and skeletons while loading

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -5,66 +5,98 @@ import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Skeleton from '@mui/material/Skeleton';
 
 const Sidebar = (props) => {
-  const { archives, social } = props;
+  const { archives, social, isLoading } = props;
 
   const socialProvided = social.some((network) => network.url !== '');
 
   return (
     <Grid item xs={12} md={4}>
-      {archives.length > 0 && (
+      {(archives.length > 0 || isLoading) && (
         <Typography component='h4' variant='h6' gutterBottom>
           Archives
         </Typography>
       )}
-      {archives?.map((archive) => (
-        <Link
-          component={NavLink}
-          display='block'
-          variant='body1'
-          underline='none'
-          to={
-            'posts/archives/' +
-            archive.title?.toLowerCase()?.split(' ')?.join('/')
-          }
-          key={archive.title}
-          sx={{
-            '&:hover': {
-              opacity: 0.75,
-            },
-          }}>
-          {archive.title}
-        </Link>
-      ))}
+      {isLoading && (
+        <>
+          <Skeleton variant='text' width='25%' />
+          <Skeleton variant='text' width='25%' />
+          <Skeleton variant='text' width='25%' />
+          <Skeleton variant='text' width='25%' />
+        </>
+      )}
+      {!isLoading &&
+        archives?.map((archive) => (
+          <Link
+            component={NavLink}
+            display='block'
+            variant='body1'
+            underline='none'
+            to={
+              'posts/archives/' +
+              archive.title?.toLowerCase()?.split(' ')?.join('/')
+            }
+            key={archive.title}
+            sx={{
+              '&:hover': {
+                opacity: 0.75,
+              },
+            }}>
+            {archive.title}
+          </Link>
+        ))}
       {socialProvided && (
         <Typography component='h4' variant='h6' gutterBottom sx={{ mt: 3 }}>
           Social
         </Typography>
       )}
-      {social.map(
-        (network) =>
-          network.url && (
-            <Link
-              component={NavLink}
-              display='block'
-              variant='body1'
-              underline='none'
-              to={network.url}
-              key={network.name}
-              sx={{
-                mb: 0.5,
-                '&:hover': {
-                  opacity: 0.75,
-                },
-              }}>
-              <Stack direction='row' spacing={1} alignItems='center'>
-                <network.icon />
-                <span>{network.name}</span>
-              </Stack>
-            </Link>
-          ),
+      {isLoading && (
+        <>
+          <Box display='flex' gap={1}>
+            <Skeleton variant='circular' width={24} height={24} />
+            <Skeleton variant='text' width='25%' />
+          </Box>
+          <Box display='flex' gap={1}>
+            <Skeleton variant='circular' width={24} height={24} />
+            <Skeleton variant='text' width='25%' />
+          </Box>
+          <Box display='flex' gap={1}>
+            <Skeleton variant='circular' width={24} height={24} />
+            <Skeleton variant='text' width='25%' />
+          </Box>
+          <Box display='flex' gap={1}>
+            <Skeleton variant='circular' width={24} height={24} />
+            <Skeleton variant='text' width='25%' />
+          </Box>
+        </>
       )}
+      {!isLoading &&
+        social.map(
+          (network) =>
+            network.url && (
+              <Link
+                component={NavLink}
+                display='block'
+                variant='body1'
+                underline='none'
+                to={network.url}
+                key={network.name}
+                sx={{
+                  mb: 0.5,
+                  '&:hover': {
+                    opacity: 0.75,
+                  },
+                }}>
+                <Stack direction='row' spacing={1} alignItems='center'>
+                  <network.icon />
+                  <span>{network.name}</span>
+                </Stack>
+              </Link>
+            ),
+        )}
     </Grid>
   );
 };
