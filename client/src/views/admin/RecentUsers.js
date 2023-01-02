6ee64080b03de93b4c655a5c836b3eda8f949a5b
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-01-02 12:48:07 +0100

    Added no users info

--- client/src/views/admin/RecentUsers.js
+++ client/src/views/admin/RecentUsers.js
@@ -5,41 +5,67 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import InfoIcon from '@mui/icons-material/Info';
 import Title from './Title';
+import { Icon } from '@mui/material';
 
 const RecentUsers = (props) => {
   const { recentUsers, isLoading } = props;
+  console.log(recentUsers);
 
   return (
     <>
       <Title>Recent users</Title>
-      <Table size='small'>
-        <TableHead>
-          <TableRow>
-            <TableCell>lp</TableCell>
-            <TableCell>Full name</TableCell>
-            <TableCell>Sign up date</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {isLoading &&
-            [...Array(4)].map((_, i) => (
+      {isLoading && (
+        <Table size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell>lp</TableCell>
+              <TableCell>Full name</TableCell>
+              <TableCell>Sign up date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {[...Array(4)].map((_, i) => (
               <TableRow key={i}>
                 <TableCell>{i}</TableCell>
                 <TableCell>{<Skeleton variant='text' />}</TableCell>
                 <TableCell>{<Skeleton variant='text' />}</TableCell>
               </TableRow>
             ))}
-          {!isLoading &&
-            recentUsers.map((user, i) => (
+          </TableBody>
+        </Table>
+      )}
+      {recentUsers.length === 0 && !isLoading && (
+        <Box display='flex' flexDirection='column' alignItems='center' gap={1}>
+          <InfoIcon sx={{ fontSize: '57.6px' }} />
+          <Typography component='h1' variant='h5'>
+            No new users!
+          </Typography>
+        </Box>
+      )}
+      {recentUsers.length > 0 && !isLoading && (
+        <Table size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell>lp</TableCell>
+              <TableCell>Full name</TableCell>
+              <TableCell>Sign up date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {recentUsers.map((user, i) => (
               <TableRow key={i}>
                 <TableCell>{i}</TableCell>
                 <TableCell>{user?.fullName}</TableCell>
                 <TableCell>{user?.created}</TableCell>
               </TableRow>
             ))}
-        </TableBody>
-      </Table>
+          </TableBody>
+        </Table>
+      )}
     </>
   );
 };
