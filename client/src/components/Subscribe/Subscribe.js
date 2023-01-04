54627cac7208f9c5e8e9b0b3c0ce58de91ded18b
HiImWhite <wojttasek1999@gmail.com>
2023-01-05 00:57:54 +0100

    Update Subscribe.js

--- client/src/components/Subscribe/Subscribe.js
+++ client/src/components/Subscribe/Subscribe.js
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import axios from 'axios';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
+});
 
 const Subscribe = () => {
+  const [emailError, setEmailError] = useState('');
+  const [email, setEmail] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleSend = async (e) => {
+    e.preventDefault();
+    setOpen(true);
+
+    if (!email) {
+      setEmailError('Field is empty.');
+      return;
+    }
+
+    if (!/@/.test(email)) {
+      setEmailError('Please enter a valid email address.');
+      return;
+    }
+
+    const data = {
+      email: email,
+    };
+
+    try {
+      await axios.post('http://localhost:8000/api/subscribe', data, {
+        withCredentials: true,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    setEmail('');
+    setEmailError('');
+  };
+
   return (
     <Container maxWidth='sm'>
       <Grid item xs={12} md={6} sx={{ mt: 3, mb: 1 }}>
@@ -41,6 +88,8 @@ const Subscribe = () => {
               <TextField
                 id='outlined-basic'
                 label='Enter email address'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 variant='outlined'
                 sx={{
                   '& label.Mui-focused': {
@@ -55,6 +104,7 @@ const Subscribe = () => {
               />
               <Button
                 variant='contained'
+                onClick={handleSend}
                 sx={{
                   textTransform: 'none',
                   backgroundColor: 'gray',
@@ -62,6 +112,18 @@ const Subscribe = () => {
                 }}>
                 Subscribe Now
               </Button>
+              <Snackbar
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                open={open}
+                autoHideDuration={4000}
+                onClose={handleClose}>
+                <Alert
+                  onClose={handleClose}
+                  severity={emailError ? 'error' : 'success'}
+                  sx={{ width: '100%' }}>
+                  {emailError ? emailError : 'Sending email success!'}
+                </Alert>
+              </Snackbar>
             </Box>
           </CardContent>
         </Card>
